f3b987d1e8c26541e0f6ef20b87685dd9b7ca3a5
we <david@wemakeprice.com>
2018-12-10 09:16:16 +0900

    data_loader

--- data_loader.py
+++ data_loader.py
@@ -33,6 +33,7 @@ import glob
 import warnings
 warnings.filterwarnings("ignore")
 
+
 class CommonDataset(Dataset):
     def __init__(self, root_dir , sub_dir, file_type, transform=None):
         """
